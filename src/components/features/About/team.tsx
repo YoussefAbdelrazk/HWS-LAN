27ee85d9323d22a5dbc8395f84f59134681f664a
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-09-22 14:26:47 +0300

    Fix : Image Team

--- src/components/features/About/team.tsx
+++ src/components/features/About/team.tsx
@@ -1,6 +1,5 @@
 import SectionTitle from '@/components/shared/sectionTitle'
-import Image from 'next/image'
-import React from 'react'
+import Image from 'next/image';
 
 export default function Team() {
   return (
@@ -8,12 +7,19 @@ export default function Team() {
       <div className='text-center space-y-4 mb-12'>
         <SectionTitle title='' text='Our Team… Behind Every Move' />
         <p className='max-w-xl mx-auto text-[#323232CC]'>
-        Our expert trainers bring passion and energy to every step, delivering an unparalleled dance fitness experience
+          Our expert trainers bring passion and energy to every step, delivering an unparalleled
+          dance fitness experience
         </p>
       </div>
       <div>
-        <Image src='/assets/header.png' alt='Team' width={500} height={500} className='rounded-lg w-full h-[250px] object-cover object-center' />
+        <Image
+          src='/assets/Header.svg'
+          alt='Team'
+          width={500}
+          height={500}
+          className='rounded-lg w-full h-[320px] object-cover object-center'
+        />
       </div>
     </div>
-  )
+  );
 }
